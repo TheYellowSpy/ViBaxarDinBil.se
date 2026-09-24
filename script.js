6a2e7f8ace866e3cb4da21dabb4e119885abfe12
TheYellowSpy <bwaarnell@gmail.com>
2026-09-24 15:20:15 +0200

    v2.1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function baxa() {
+    document.getElementById("vanta").innerHTML = "Vänta";
     document.getElementById("vanta").style.opacity = 1;
     setTimeout(function() {
         document.getElementById("vanta").innerHTML = "<b> Baxad! </b>";
